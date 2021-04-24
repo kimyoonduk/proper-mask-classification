6eecf408c1a0ff3b44f17e3e165982b55550c835
Yoonduk Kim <ydpublic@gmail.com>
2021-04-24 17:18:37 +0900

    kaggle data for correct mask

--- model/kaggle_data.py
+++ model/kaggle_data.py
@@ -82,104 +82,110 @@ def crop_face(datarow, newsize):
     return (im, img_dim)
 
 
-anno_folder = "../data/kaggle/annotations/"
-img_folder = "../data/kaggle/images/"
-save_folder = "../data/kaggle/cropped/"
+def main():
 
-anno_paths = sorted(list(paths.list_files(anno_folder)))
-anno_paths = [ap for ap in anno_paths if ap.endswith(".xml")]
+    anno_folder = "../data/kaggle/annotations/"
+    img_folder = "../data/kaggle/images/"
+    save_folder = "../data/kaggle/new/"
 
-img_paths = sorted(list(paths.list_images(img_folder)))
+    anno_paths = sorted(list(paths.list_files(anno_folder)))
+    anno_paths = [ap for ap in anno_paths if ap.endswith(".xml")]
 
-print(len(anno_paths))
+    img_paths = sorted(list(paths.list_images(img_folder)))
 
-imgids = []
-labels = []
-boxes = []
-widths = []
-heights = []
+    print(len(anno_paths))
 
-# no mask: 52
-# len([lb for lb in labels if lb == 0])
+    imgids = []
+    labels = []
+    boxes = []
+    widths = []
+    heights = []
 
+    # no mask: 52
+    # len([lb for lb in labels if lb == 0])
 
-for i, anno_path in tqdm(enumerate(anno_paths), total=len(anno_paths)):
-    with open(anno_path) as f:
-        data = f.read()
-        soup = BeautifulSoup(data, "xml")
-        objects = soup.find_all("object")
-        imgid = re.match(r".+\/(.+)\.xml", anno_path)[1]
+    for i, anno_path in tqdm(enumerate(anno_paths), total=len(anno_paths)):
+        with open(anno_path) as f:
+            data = f.read()
+            soup = BeautifulSoup(data, "xml")
+            objects = soup.find_all("object")
+            imgid = re.match(r".+\/(.+)\.xml", anno_path)[1]
 
-        for obj in objects:
-            box = generate_box(obj)
-            box_width = box[2] - box[0]
-            box_height = box[3] - box[1]
+            for obj in objects:
+                box = generate_box(obj)
+                box_width = box[2] - box[0]
+                box_height = box[3] - box[1]
 
-            imgids.append(imgid)
-            widths.append(box_width)
-            heights.append(box_height)
-            labels.append(generate_label(obj))
-            boxes.append(box)
+                imgids.append(imgid)
+                widths.append(box_width)
+                heights.append(box_height)
+                labels.append(generate_label(obj))
+                boxes.append(box)
 
-kaggle_dict = {
-    "id": imgids,
-    "label": labels,
-    "box": boxes,
-    "width": widths,
-    "height": heights,
-}
+    kaggle_dict = {
+        "id": imgids,
+        "label": labels,
+        "box": boxes,
+        "width": widths,
+        "height": heights,
+    }
 
-kaggle_df = pd.DataFrame.from_dict(kaggle_dict)
+    kaggle_df = pd.DataFrame.from_dict(kaggle_dict)
 
-# total number of faces in dataset
-len(kaggle_df)
+    # total number of faces in dataset
+    len(kaggle_df)
 
-print("distribution of widths and heights for face area box")
-sns.distplot(kaggle_df["width"])
-sns.distplot(kaggle_df["height"])
+    print("distribution of widths and heights for face area box")
+    sns.distplot(kaggle_df["width"])
+    sns.distplot(kaggle_df["height"])
 
-# filter incorrectly worn examples
-filtered = kaggle_df[kaggle_df["label"] != 2]
+    # filter incorrectly worn examples
+    filtered = kaggle_df[kaggle_df["label"] != 2]
 
-# face should be large enough
-filtered = filtered[filtered["height"] >= 80]
+    # face should be large enough
+    filtered = filtered[filtered["height"] >= 60]
 
-# photograph should contain only one person
-filtered = filtered[filtered["label"].duplicated(keep=False)]
+    # photograph should contain only one person
+    filtered = filtered[filtered["label"].duplicated(keep=False)]
 
-filtered.pivot_table(index="label", aggfunc=len)
+    # TEMP: filtering out data from previous iteration
+    # v1 = filtered[filtered["cropped_dim"] != (0, 0)]
+    # v1_list = v1["id"]
+    # filtered = filtered[~filtered["id"].isin(v1_list)]
 
+    filtered.pivot_table(index="label", aggfunc=len)
 
-newsize = 300
-dim_list = []
+    newsize = 200
+    dim_list = []
 
-for idx, row in filtered.iterrows():
-    new_img, img_dim = crop_face(row, newsize)
-    if img_dim != (0, 0):
-        new_img = new_img.convert("RGB")
-        img_id = row["id"]
-        label_str = "cw_" if row["label"] == 1 else "nm_"
-        save_path = save_folder + label_str + img_id + ".jpg"
-        new_img.save(save_path, "JPEG")
-    dim_list.append(img_dim)
+    for idx, row in filtered.iterrows():
+        new_img, img_dim = crop_face(row, newsize)
+        if img_dim != (0, 0):
+            new_img = new_img.convert("RGB")
+            img_id = row["id"]
+            label_str = "cw_" if row["label"] == 1 else "nm_"
+            save_path = save_folder + label_str + img_id + ".jpg"
+            new_img.save(save_path, "JPEG")
+        dim_list.append(img_dim)
 
+    filtered["cropped_dim"] = dim_list
+    filtered[filtered["cropped_dim"] != (0, 0)].pivot_table(index="label", aggfunc=len)
 
-## TEST
-datarow = filtered.iloc[1]
+    ## BOX TEST ##
+    datarow = filtered.iloc[1]
 
-img_path = img_folder + datarow["id"] + ".png"
-im = Image.open(img_path)
+    img_path = img_folder + datarow["id"] + ".png"
+    im = Image.open(img_path)
 
-box = temp["box"]
+    box = temp["box"]
 
-box_path = [
-    (box[0], box[1]),
-    (box[0], box[3]),
-    (box[2], box[3]),
-    (box[2], box[1]),
-    (box[0], box[1]),
-]
+    box_path = [
+        (box[0], box[1]),
+        (box[0], box[3]),
+        (box[2], box[3]),
+        (box[2], box[1]),
+        (box[0], box[1]),
+    ]
 
-d = ImageDraw.Draw(im, "RGBA")
-d.line(box_path, fill=(150, 0, 0, 180), width=2)
-
+    d = ImageDraw.Draw(im, "RGBA")
+    d.line(box_path, fill=(150, 0, 0, 180), width=2)
